1953c132e8b30d5e4e090162456420a18f906453
Belloshehu <belloshehu1@gmail.com>
2022-09-15 13:59:43 +0100

    Add feature to decrese round when game is played.
    Resolve #11

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useGlobalContext } from '../context'
 
 const Play = () => {
+  const {setRound} = useGlobalContext()
   const handlePlayGame = (e)=>{
-    
+    // update round state value
+    // decrease round by 1 if round value
+    // is greater than 0, otherwise return 0
+    setRound(preVal => preVal > 0 ? preVal - 1: preVal -0)
   }
   return (
     <>
