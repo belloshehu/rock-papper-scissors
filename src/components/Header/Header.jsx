2f4d888343c3b562c7b75cbd88de556d1f222f98
Belloshehu <belloshehu1@gmail.com>
2022-09-15 13:19:17 +0100

    Add context provider for score, round, setScore, and setRound contexts
    See: #10

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,30 @@
 import React from 'react'
 import { useState } from 'react'
+import { AppContext } from '../../context'
+import { useGlobalContext } from '../../context'
 
 export const Header = () => {
-  const [userScore, setUserScore]  = useState(0)
-  const [computerScore, setcomputerrScore]  = useState(0)
-
-
+  const {score, round} = useGlobalContext() 
   return (
   <React.Fragment>
     <div className='box'>
       <div className='colume-info'>
-      <div className='flex-name'>
-        <span colume-computer>
-        <h4 className='computer-color'>Computer</h4>
-        <span>0</span>
-        </span>
-      
-        <span colume-computer>
-          <h4 className='user-color'>user</h4>
-          <span>0</span>
-        </span>
-      </div>
+        <div className='flex-name'>
+          <span colume-computer>
+            <h4 className='computer-color'>Computer</h4>
+            <span>{score.computer}</span>
+          </span>
+        
+          <span colume-computer>
+            <h4 className='user-color'>user</h4>
+            <span>{score.user}</span>
+          </span>
+        </div>
 
-      <div className='round-border'>
-      <span className='round'> <h3>Round: </h3></span>
+        <div className='round-border'>
+          <span className='round'> <h3>Round {round}</h3></span>
+        </div>
       </div>
-      </div>
-
     </div>
   </React.Fragment>
   )
