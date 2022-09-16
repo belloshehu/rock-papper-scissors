f51a7b5ffea18d54d922cf94bb3653d7074fb7ad
Belloshehu <belloshehu1@gmail.com>
2022-09-16 17:36:44 +0100

    Improve Header
    Resolve #13

--- src/collection.js
+++ src/collection.js
@@ -1,15 +1,22 @@
+import scissors from './images/scissors.png'
+import rock from './images/rock.jpg'
+import paper from './images/paper.jpg'
+
 const collection = [
     {
         text: "Rock",
-        rank: 0
+        rank: 0,
+        image: rock
     },
     {
         text: "Paper",
-        rank: 1
+        rank: 1,
+        image: paper
     },
     {
         text: "Scissors",
-        rank: 2
+        rank: 2,
+        image: scissors
     }
 ]
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,29 +2,43 @@ import React from 'react'
 import { useState } from 'react'
 import { AppContext } from '../../context'
 import { useGlobalContext } from '../../context'
+import FaComputer, { FaCrown, FaDesktop, FaUser } from 'react-icons/fa'
+
+
+
 
 export const Header = () => {
   const {score, round, choice} = useGlobalContext() 
   return (
   <React.Fragment>
     <div className='box'>
-      <div className='colume-info'>
         <div className='flex-name'>
-          <span>
-            <h4 className='computer-color'>Computer: {choice.computer.text}</h4>
-            <span>{score.computer}</span>
-          </span>
+          <div className='player-wrapper computer-wrapper'>
+            <FaDesktop className='player'/>
+            <div className='img-wrapper'>
+              <img className='img-choice' src={choice.computer.image}/>
+            </div>
+            <p>{score.computer}</p>
+            <div>
+              <FaCrown/>
+            </div>
+          </div>
         
-          <span>
-            <h4 className='user-color'>user: {choice.user.text}</h4>
-            <span>{score.user}</span>
-          </span>
+          <div className='player-wrapper user-wrapper'>
+            <FaUser className='player' />
+            <div className='img-wrapper'>
+              <img className='img-choice' src={choice.user.image}/>
+              </div>
+            <p>{score.user}</p>
+            <div>
+              <FaCrown/>
+            </div>
+          </div>
         </div>
 
         <div className='round-border'>
           <span className='round'> <h3>Round {round}</h3></span>
         </div>
-      </div>
     </div>
   </React.Fragment>
   )
